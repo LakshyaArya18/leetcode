54c737846de26f929c7178e3d6c9aeb4af3a990f
LakshyaArya18 <lakshyaarya1289@gmail.com>
2026-07-31 14:51:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (36.60%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long val = 1;
+
+        for (int j = 0; j <= rowIndex; j++) {
+            row.add((int) val);
+            val = val * (rowIndex - j) / (j + 1);
+        }
+
+        return row;
+    }
+}
